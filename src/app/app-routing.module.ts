d3263994a39348b0b729aba1a7b1c49fa023aa43
syraxxxx <rsitraka181@gmail.com>
2023-01-31 13:19:20 +0300

    creating  inscription component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LoginService } from './security/login/login.service';
 import { LogoutService } from './security/logout/logout.service';
+import {InscriptionComponent} from "./inscription/inscription.component";
 
 const routes: Routes = [
   {
     path: '',
     component: LoginComponent,
+    // canActivate: [LogoutService]
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    // canActivate: [LogoutService]
+  },
+  {
+    path: 'signIn',
+    component: InscriptionComponent,
     // canActivate: [LogoutService]
   },
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    InscriptionComponent
   ],
   imports: [
     BrowserModule,
