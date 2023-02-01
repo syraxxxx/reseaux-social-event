59ae97cbcdf0271b25df9f36e5bfa0580cc58e1d
syraxxxx <rsitraka181@gmail.com>
2023-02-01 18:39:50 +0300

    inscription formulaire

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-inscription',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InscriptionComponent implements OnInit {
 
+  public formInscription = new FormGroup({
+    nom: new FormControl('', [Validators.required]),
+    prenom: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+    tel: new FormControl('', [Validators.required]),
+    mot_de_passe: new FormControl('', [Validators.required]),
+    pays: new FormControl('', [Validators.required]),
+    ville: new FormControl('', [Validators.required]),
+    profil_photo: new FormControl('')
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  signIn(){
+
   }
 
 }
