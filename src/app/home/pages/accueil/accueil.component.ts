04d482b13396cc8fa4efabc1b1032196e0f8205d
syraxxxx <rsitraka181@gmail.com>
2023-02-05 18:30:28 +0300

    list publication done

--- src/app/home/pages/accueil/accueil.component.ts
+++ src/app/home/pages/accueil/accueil.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PublicationService} from "../../../@core/services/publication.service";
 
 @Component({
   selector: 'app-accueil',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accueil.component.scss']
 })
 export class AccueilComponent implements OnInit {
+  publications: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private postService: PublicationService
+  ) {
   }
 
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData() {
+    this.postService.getListePublication().subscribe(response => {
+      this.publications = response.publication;
+      console.log(response.publication);
+    })
+  }
 }
