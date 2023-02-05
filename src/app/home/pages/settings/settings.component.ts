9f51d70014a4229936089e194de48f1bfbcf0f50
syraxxxx <rsitraka181@gmail.com>
2023-02-05 22:57:36 +0300

    set up front to settings

--- src/app/home/pages/settings/settings.component.ts
+++ src/app/home/pages/settings/settings.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  formMail = new FormGroup({
+    user_id: new FormControl('', [Validators.required]),
+    old_mail: new FormControl('', [Validators.required]),
+    new_mail: new FormControl('', [Validators.required]),
+  });
+  formMdp = new FormGroup({
+    user_id: new FormControl('', [Validators.required]),
+    old_mdp: new FormControl('', [Validators.required]),
+    new_mdp: new FormControl('', [Validators.required]),
+  });
+  formDesactivate = new FormGroup({
+    user_id: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+    mdp: new FormControl('', [Validators.required]),
+    description: new FormControl('', [Validators.required]),
+  });
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  changeMail() {
+    console.log('change mail');
+  }
+
+  changeMdp() {
+    console.log('change mdp');
+  }
+
+  desactivateAccount() {
+    console.log('desactivate account');
   }
 
 }
