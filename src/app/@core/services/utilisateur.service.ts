42c4e37fbd4943ede5c7ab7c9c032da840f6e6ad
Steve <ramiandrisoasteve@gmail.com>
2023-02-07 10:38:38 +0300

    inscription

--- src/app/@core/services/utilisateur.service.ts
+++ src/app/@core/services/utilisateur.service.ts
@@ -17,19 +17,14 @@ export class UtilisateurService {
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/x-www-form-urlencoded'
+        // 'Content-Type': 'application/form-data'
       })
     };
-    
-    const data = `email=${encodeURIComponent('example@email.com')}&
-    nom=${encodeURIComponent('Example')}&
-    prenom=${encodeURIComponent('User')}&
-    mdp=${encodeURIComponent('password')}&
-    tel=${encodeURIComponent('1234567890')}&
-    pays=${encodeURIComponent('Country')}&
-    ville=${encodeURIComponent('City')}&
-    profil_photo=${encodeURIComponent('photo.jpg')}`;
 
-    console.log(data);
+    const data = `email=${encodeURIComponent('example@email.com')}&nom=${encodeURIComponent('Example')}&prenom=${encodeURIComponent('User')}&mdp=${encodeURIComponent('password')}&tel=${encodeURIComponent('1234567890')}&pays=${encodeURIComponent('Country')}&ville=${encodeURIComponent('City')}&profil_photo=${encodeURIComponent('photo.jpg')}`;
+
+    // console.log(data);
+    // console.log('here');
     return await this.http.post<any>(`${this.apiEndPoint}/inscription`,data,httpOptions);
   }
   // create(body: any): Observable<any> {
