61f1606c45344face2ef61ab40bea10f77104771
syraxxxx <rsitraka181@gmail.com>
2023-02-14 00:52:21 +0300

    rectification result

--- src/app/home/pages/result-search/result-search.component.ts
+++ src/app/home/pages/result-search/result-search.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {SearchService} from "../../../@core/services/search.service";
 import {NgxSpinnerService} from "ngx-spinner";
@@ -18,41 +18,20 @@ export class ResultSearchComponent implements OnInit {
     private router: Router,
     private searchService: SearchService,
     private spinner: NgxSpinnerService,
-    private cd: ChangeDetectorRef
   ) {
   }
 
   ngOnInit(): void {
     this.spinner.show();
-    // this.search = this.route.snapshot.paramMap.get('search');
-    // this.result = this.route.snapshot.paramMap.get('valeurRecherche');
-    console.log(this.search);
-    console.log(this.result);
-    // console.log(this.route.snapshot.paramMap.get('valeurRecherche'));
-    // this.getResult();
-    this.route.params.subscribe(params=>{
+    this.route.params.subscribe(params => {
       this.search = params['search']
-      console.log("params : "+params['search']);
-      this.updateData(params['search']);
+      this.getResult(params['search']);
     })
-
   }
 
-  updateData(params :any) {
+  getResult(params: any) {
     this.searchService.search(params).subscribe(response => {
-      console.log(response);
       this.result = response;
-    })
-  }
-
-  getResult() {
-    console.log(this.search);
-    this.searchService.search(this.search).subscribe(response => {
-      console.log(response);
-      this.result = response;
-      // setTimeout(() => {
-      //   this.spinner.hide();
-      // }, 500);
     })
   }
 
