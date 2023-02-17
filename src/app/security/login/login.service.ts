ca0fc069e4cfa55029a1e7f477f6fb94e8a5f366
syraxxxx <rsitraka181@gmail.com>
2023-02-17 15:41:51 +0300

    can activate in admin and user

--- src/app/security/login/login.service.ts
+++ src/app/security/login/login.service.ts
@@ -1,34 +1,41 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {CanActivate, Router} from '@angular/router';
 import {UtilisateurService} from "../../@core/services/utilisateur.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class LoginService implements CanActivate{
+export class LoginService implements CanActivate {
 
   constructor(
     private router: Router,
-    private userService : UtilisateurService
-  ) { }
+    private userService: UtilisateurService
+  ) {
+  }
 
   canActivate(): boolean {
-    if(!localStorage.getItem('token')) this.router.navigateByUrl('/')
-    return true;
-  }
-  canActivateAdmin(): boolean {
-    if(!localStorage.getItem('token')) this.router.navigateByUrl('/')
+    if (!localStorage.getItem('token')) {
+      this.router.navigateByUrl('/')
+    }
+    this.userService.getUserByToken().subscribe(response => {
+      if (response.user[0].admin == 1) {
+        this.router.navigateByUrl('/admin')
+      } else if (response.user[0].admin == 0) {
+        this.router.navigateByUrl('/home')
+      } else {
+        this.router.navigateByUrl('/home')
+      }
+    });
     return true;
   }
 
   public login(token: string) {
     localStorage.setItem('token', token);
 
-    this.userService.getUserByToken().subscribe(response=>{
-      if(response.user[0].admin==1){
+    this.userService.getUserByToken().subscribe(response => {
+      if (response.user[0].admin == 1) {
         this.router.navigateByUrl('/admin')
-      }else{
+      } else {
         this.router.navigateByUrl('/home')
       }
     })
