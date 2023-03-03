f5462999bca52b69816c1112c4bcdb7b6ec5e4ed
syraxxxx <rsitraka181@gmail.com>
2023-03-03 22:52:22 +0300

    Update home-routing.module.ts

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -5,7 +5,6 @@ import {HomeComponent} from './home.component';
 import {ProfileComponent} from "./pages/profile/profile.component";
 import {MessagesComponent} from "./pages/messages/messages.component";
 import {DiscussionComponent} from "./pages/discussion/discussion.component";
-import {EventComponent} from "./pages/event/event.component";
 import {SettingsComponent} from "./pages/settings/settings.component";
 import {NewEventComponent} from "./pages/event/new-event/new-event.component";
 import {DetailsEventComponent} from "./pages/event/details-event/details-event.component";
